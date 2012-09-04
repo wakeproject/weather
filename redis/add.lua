28434d2aa69c73d66e43d45b9e9be93fc854e362
Mingli Yuan <mingli.yuan@gmail.com>
2012-09-04 09:48:03 +0800

    finish first version of add and prf

--- redis/add.lua
+++ redis/add.lua
@@ -14,7 +14,28 @@ for idx = 1, dim do
     all = all * lyt[idx]
 end
 
-while all do
-    all = all - 1
+while all != 0 do
+    total = all
+    affix = ''
+    for idx = dim, 1, -1 do
+        reminder = total % lyt[idx]
+        total = total - reminder - lyt[idx]
+        if reminder == 0
+            reminder = lyt[idx]
+        end
+        affix = ':' .. reminder .. affix
 
+        key1 = src1 .. affix
+        key2 = src2 .. affix
+        key3 = trgt .. affix
+
+        opd1 = redis.call('lrange', key1, 0, -1)
+        opd2 = redis.call('lrange', key2, 0, -1)
+
+        for ind = dim, 1, -1 do
+            result = opd1[ind] + opd2[ind]
+            redis.call('lpush', key3, result)
+        end
+    end
+    all = all - 1
 end
--- redis/prd.lua
+++ redis/prd.lua
@@ -14,7 +14,26 @@ for idx = 1, dim do
     all = all * lyt[idx]
 end
 
-while all do
-    all = all - 1
+while all != 0 do
+    total = all
+    affix = ''
+    for idx = dim, 1, -1 do
+        reminder = total % lyt[idx]
+        total = total - reminder - lyt[idx]
+        if reminder == 0
+            reminder = lyt[idx]
+        end
+        affix = ':' .. reminder .. affix
 
+        key1 = src1 .. affix
+        key2 = trgt .. affix
+
+        opd1 = redis.call('lrange', key1, 0, -1)
+
+        for ind = dim, 1, -1 do
+            result = opd1[ind] * cnst
+            redis.call('lpush', key2, result)
+        end
+    end
+    all = all - 1
 end
